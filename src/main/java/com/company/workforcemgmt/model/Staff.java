2dd787435f0e4bab2a23f600200d85b66bacce75
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-08-05 11:48:53 +0530

    Create Staff model class with Lombok annotations

---
+++ src/main/java/com/company/workforcemgmt/model/Staff.java
@@ -0,0 +1,15 @@
+package com.company.workforcemgmt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Staff {
+    private String id;
+    private String name;
+    private String email;
+    private String department;
+}
